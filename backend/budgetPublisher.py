f85213cf7ec924ad5ad8af4287ec1e6eb849f849
miguel5andrade <miguel.andrade.net@gmail.com>
2025-03-29 14:46:03 +0000

    fixes in budgetPublisher

--- backend/budgetPublisher.py
+++ backend/budgetPublisher.py
@@ -57,20 +57,11 @@ def on_publish(client, userdata, mid, reason_code, properties):
 
 def on_disconnect(client, userdata, rc, properties=None ,reasonCode=None ):
     print(f"Disconnected from broker, with code {rc}...")
-    #retry to connect
-    while(1):
-        try:
-            client.reconnect()
-            print("Reconnected successfully!")
-            return
-        except Exception as err:
-            print("%s. Reconnect failed. Retrying...", err)
-        time.sleep(1)
 
 
 def push_budget_message():
     # Create MQTT client with proper settings for MQTTv5
-    client = mqtt.Client(client_id="deepwatt_subscriber2", protocol=mqtt.MQTTv5, callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+    client = mqtt.Client(client_id="deepwatt_subscriber33", protocol=mqtt.MQTTv5, callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
 
     # Assign event callbacks
     client.on_connect = on_connect
@@ -82,7 +73,7 @@ def push_budget_message():
     
     # Connect to broker
     client.connect(BROKER_ADDRESS, int(BROKER_PORT))
-
+    client.loop_start()
     session = Session()
 
     budgets = session.query(Budget).all()
@@ -91,12 +82,13 @@ def push_budget_message():
         last_reading = session.query(DeviceReading).filter_by(device_id = record.monitoring_device_id).order_by(DeviceReading.id.desc()).first()
         usage_percentage = last_reading.dailyEnergy / record.budget * 100
         print(usage_percentage)
-        topic = "budget/board-" + record.feedback_device_id
+        topic = "budget/" + record.feedback_device_id
         payload = int(usage_percentage)
         client.publish(topic=topic,payload= payload, qos=0, retain=0, properties=None)
-        print(f"topic: {topic} \t, payload: {payload}")
-
-
+        print(f"topic: {topic}, payload: {payload}")
+        time.sleep(1)
+	
+    client.loop_stop()
     client.disconnect()
     
 if __name__ == '__main__':
