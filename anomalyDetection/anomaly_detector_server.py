58d5a06e98d666bc50fa92092956521e2c24d985
miguel5andrade <miguel.andrade.net@gmail.com>
2025-05-28 23:00:55 +0100

    added ML model for anomaly detection

---
+++ anomalyDetection/anomaly_detector_server.py
@@ -0,0 +1,76 @@
+import sqlite3
+import pandas as pd
+import joblib
+from datetime import timedelta
+import time
+import logging 
+
+DB_PATH = 'deepwatt.db'
+MODEL_PATH = 'anomaly_model.pkl'
+SCALER_PATH = 'scaler.pkl'
+DEVICE_ID = 'board-cc:50:e3:60:e6:80'  
+
+# Load the model and the scaler
+model = joblib.load(MODEL_PATH)
+scaler = joblib.load(SCALER_PATH)
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',filename='anomaly-detector.log', level=logging.INFO)
+
+# time period of 5 min
+now = time.time()
+start_time = int((now - timedelta(minutes=5)).timestamp())
+end_time = int(now.timestamp())
+
+
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
+
+query = """
+SELECT id, timestamp, rms_current
+FROM device_readings
+WHERE device_id = ? AND timestamp >= ? AND timestamp <= ? AND rms_current IS NOT NULL
+ORDER BY timestamp ASC
+"""
+df = pd.read_sql_query(query, conn, params=(DEVICE_ID, start_time, end_time))
+
+if df.empty:
+    logging.warning("No new data in the last 5 min.")
+    conn.close()
+    exit()
+
+# === Pre processing ===
+df['datetime'] = pd.to_datetime(df['timestamp'], unit='s')
+df.set_index('datetime', inplace=True)
+df['current_scaled'] = scaler.transform(df[['rms_current']])
+
+# === Anomaly detection ===
+df['anomaly'] = model.predict(df[['current_scaled']])  # -1 = anomaly
+anomalies = df[df['anomaly'] == -1]
+
+# Insert anomalies in the db  
+inserted = 0
+for _, row in anomalies.iterrows():
+    # check if this anomaly was already inserted
+    cursor.execute("SELECT COUNT(*) FROM anomalies WHERE device_reading_id = ?", (int(row['id']),))
+    exists = cursor.fetchone()[0]
+
+    if exists == 0:
+        #if not, insert
+        cursor.execute("""
+            INSERT INTO anomalies (device_reading_id, rms_current, timestamp, created_at)
+            VALUES (?, ?, ?, ?)
+        """, (
+            int(row['id']),
+            float(row['rms_current']),
+            int(row['timestamp']),
+            time.time()
+        ))
+        inserted += 1
+
+conn.commit()
+conn.close()
+
+if inserted:
+    logging.info(f"Inserted {inserted} new anomalies.")
+else:
+    logging.info("No new anomalies.")
